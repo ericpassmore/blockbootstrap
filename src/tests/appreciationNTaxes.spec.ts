ff3bd77484a979a816483d844d1c5c7bba8895ee
Eric Passmore <eric.passmore@gmail.com>
2025-08-03 09:27:15 -0700

    test appreciation and assets

--- src/tests/appreciationNTaxes.spec.ts
+++ src/tests/appreciationNTaxes.spec.ts
@@ -19,7 +19,7 @@ describe('Taxes', () => {
             expect(model.results[0].taxes, `First year taxes for block ${blockNumber} should be > 0`).toBeGreaterThan(0);
         }
     })
-    it('check taxes for block 9 with 100% Treasuries', () => {
+    it('check taxes/appreciation for block 9 with 100% Treasuries', () => {
                 const allocations: Allocation[] = [
             { key: 'treasury10Year', label: '10-Year Treasury', value: 100 }
         ];
@@ -28,9 +28,11 @@ describe('Taxes', () => {
 
         // We expect some ordinary income from treasuries each year, which should result in taxes.
         const totalTaxes = model.results.reduce((sum, yearResult) => sum + yearResult.taxes, 0);
-        expect(totalTaxes, `Total taxes for block 9 should be close to $8682.34`).toBeCloseTo(8682.34,2);
+        expect(totalTaxes, `Taxes for block 9 with 100% Treasuries should be close to $8682.34`).toBeCloseTo(8682.34,2);
+        const terminatingValue = model.results[9].endValue;
+        expect(terminatingValue, `Terminating value for block 9 with 100% Treasuries`).toBeCloseTo(242664.40,2);
     })
-    it('check taxes for block 9 with 100% baaCorp', () => {
+    it('check taxes/appreciation for block 9 with 100% baaCorp', () => {
                 const allocations: Allocation[] = [
             { key: 'baaCorp', label: 'BAA Corporate Bonds', value: 100 }
         ];
@@ -39,6 +41,21 @@ describe('Taxes', () => {
 
         // We expect some ordinary income from treasuries each year, which should result in taxes.
         const totalTaxes = model.results.reduce((sum, yearResult) => sum + yearResult.taxes, 0);
-        expect(totalTaxes, `Total taxes for block 9 should be close to $8682.34`).toBeCloseTo(10112.48,2);
+        expect(totalTaxes, `Total taxes for block 9 with 100% baaCorp should be close to $8682.34`).toBeCloseTo(10112.48,2);
+        const terminatingValue = model.results[9].endValue;
+        expect(terminatingValue, `Terminating value for block 9 with 100% baaCorp `).toBeCloseTo(283910.25,2);
+    })
+    it('check taxes/appreciation block 4 with 100% sp500', () => {
+                const allocations: Allocation[] = [
+            { key: 'sp500', label: 'S&P 500', value: 100 }
+        ];
+        const startingAmount = 100000;
+        const model = new ModelReturns(startingAmount, allocations, 4);
+
+        // We expect some ordinary income from treasuries each year, which should result in taxes.
+        const totalTaxes = model.results.reduce((sum, yearResult) => sum + yearResult.taxes, 0);
+        expect(totalTaxes, `Total taxes for block 4 with 100% sp500should be close to $8682.34`).toBeCloseTo(10862.77,2);
+        const terminatingValue = model.results[9].endValue;
+        expect(terminatingValue, `Terminating value for block 4 with 100% sp500 191232.78`).toBeCloseTo(191232.78,2);
     })
 })
