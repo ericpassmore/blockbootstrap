67e7b2bdbb20629b177270b4080817aa6cbef8a4
Eric Passmore <eric.passmore@gmail.com>
2025-08-04 18:03:15 -0700

    better setup schema

--- src/db/setup.sql
+++ src/db/setup.sql
@@ -1,14 +1,12 @@
 -- Create a role for the application
 CREATE ROLE blockbootstrap_user WITH LOGIN PASSWORD 'secure_password';
 
--- Grant necessary privileges to the role
-GRANT ALL PRIVILEGES ON DATABASE blockbootstrap_db TO blockbootstrap_user;
-
 -- Create the database
-CREATE DATABASE blockbootstrap_db OWNER blockbootstrap_user;
-
+CREATE DATABASE blockbootstrap_db
 -- Connect to the database to create the table
 \c blockbootstrap_db
+-- grant public schema 
+GRANT CREATE ON SCHEMA public TO blockbootstrap_user;
 
 -- Create the users table
 CREATE TABLE users (
@@ -18,6 +16,3 @@ CREATE TABLE users (
     last_login_date TIMESTAMP,
     code INTEGER CHECK (code >= 100000 AND code <= 999999) NOT NULL
 );
-
--- Grant privileges on the table to the role
-GRANT ALL PRIVILEGES ON TABLE users TO blockbootstrap_user;
