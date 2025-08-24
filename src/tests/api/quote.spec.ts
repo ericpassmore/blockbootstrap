731619ac1d364118cb6bdce26b4f86d6bbef1038
Eric Passmore <eric.passmore@gmail.com>
2025-08-23 18:33:06 -0700

    move api/quote to api/marketdata/quote

--- src/tests/api/quote.spec.ts
+++ src/tests/api/quote.spec.ts
@@ -7,11 +7,13 @@ vi.mock('$env/static/private', () => ({
 	// Add other private environment variables here
 }));
 setupPolygonServiceMock();
-import { GET } from '../../routes/api/quote/[ticker]/+server';
+import { GET } from '../../routes/api/marketdata/quote/[ticker]/+server';
 
-describe('GET /api/quote/[ticker]', () => {
+describe('GET /api/marketdata/quote/[ticker]', () => {
 	it('returns 3 or more data rows and metadata.ticker is MSFT for valid request', async () => {
-		const url = new URL('http://localhost:5173/api/quote/msft?from=2025-07-20&to=2025-08-17');
+		const url = new URL(
+			'http://localhost:5173/api/marketdata/quote/msft?from=2025-07-20&to=2025-08-17'
+		);
 		const params = { ticker: 'msft' };
 
 		const response = await GET({ url, params } as any);
@@ -23,7 +25,9 @@ describe('GET /api/quote/[ticker]', () => {
 	});
 
 	it('returns 400 error for malformed date parameters', async () => {
-		const url = new URL('http://localhost:5173/api/quote/msft?from="2025-07-20"&to="2025-08-17"');
+		const url = new URL(
+			'http://localhost:5173/api/marketdata/quote/msft?from="2025-07-20"&to="2025-08-17"'
+		);
 		const params = { ticker: 'msft' };
 
 		const response = await GET({ url, params } as any);
